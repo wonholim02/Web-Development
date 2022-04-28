394646a22c59493540a61d73a7cf9c0c2e03769b
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2022-04-28 01:46:52 -0400

    Add files via upload

--- Basic Practice/main.js
+++ Basic Practice/main.js
@@ -1,2 +1,1 @@
-document.querySelector("h1").innerHTML = 'Most popular TV show searches in 2016';
-document.getElementById('fourth').innerHTML = 'Fourth element'
+document.body.innerHTML="This is the text of the body element";
